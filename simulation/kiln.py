36dbefefd7e9121cf9f524b97bd465d794190888
Sebastian Kimberk <skimberk1@gmail.com>
2020-08-09 23:02:06 -0400

    more complex kiln

--- simulation/kiln.py
+++ simulation/kiln.py
@@ -1,23 +1,33 @@
 class Kiln:
-	max_temperature = 2300
+	max_element_temperature = 2600
 
 	def __init__(self):
 		self.temperature = 0
+		self.element_temperature = 0
 		self.on = False
 
 	def tick(self):
 		new_temperature = self.temperature
-		new_temperature -= self.temperature / self.max_temperature
+		new_temperature -= 0.001 * self.temperature
+
+		new_element_temperature = self.element_temperature
+		new_element_temperature += 0.1 * (self.temperature - self.element_temperature)
 
 		if self.on:
-			new_temperature += (self.max_temperature - self.temperature) / self.max_temperature + 1
+			new_element_temperature += 20  * (1 - self.element_temperature / self.max_element_temperature)
+
+		new_temperature += 0.01 * (self.element_temperature - self.temperature)
 
 		self.temperature = new_temperature
+		self.element_temperature = new_element_temperature
 
 if __name__ == '__main__':
 	kiln = Kiln()
 	kiln.on = True
 
-	for _ in range(2000):
-		print(kiln.temperature)
+	for _ in range(1000):
+		print('Kiln:', kiln.temperature)
+		print('Element: ', kiln.element_temperature)
+		print()
+
 		kiln.tick()
