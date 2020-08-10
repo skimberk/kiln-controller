5383a03e1c21af996c08e2d48b86504b0a023516
Sebastian Kimberk <skimberk1@gmail.com>
2020-08-09 22:37:19 -0400

    basic kiln simulator

---
+++ simulation/kiln.py
@@ -0,0 +1,22 @@
+class Kiln:
+	max_temperature = 2300
+
+	def __init__(self):
+		self.temperature = 0
+		self.on = False
+
+	def tick(self):
+		new_temperature = self.temperature
+		new_temperature -= self.temperature / self.max_temperature
+
+		if self.on:
+			new_temperature += (self.max_temperature - self.temperature) / self.max_temperature + 1
+
+		self.temperature = new_temperature
+
+kiln = Kiln()
+kiln.on = True
+
+for _ in range(2000):
+	print(kiln.temperature)
+	kiln.tick()
