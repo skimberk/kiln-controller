a214c122d7c2aba46047ad17256417930e2eb881
Sebastian Kimberk <skimberk1@gmail.com>
2020-08-09 23:06:35 -0400

    Better control logging

--- simulation/control.py
+++ simulation/control.py
@@ -3,10 +3,11 @@ from kiln import Kiln
 kiln = Kiln()
 
 for _ in range(600):
-	if kiln.temperature < 500:
+	if kiln.temperature < 100:
 		kiln.on = True
 	else:
 		kiln.on = False
 
 	kiln.tick()
-	print(kiln.on, kiln.temperature)
+
+	print('On' if kiln.on else 'Off', 'Kiln', kiln.temperature, 'Element', kiln.element_temperature)
