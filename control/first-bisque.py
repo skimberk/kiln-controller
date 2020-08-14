fbaa817d7ca6dd6dbb92621ce93230de79c23345
Sebastian Kimberk <skimberk1@gmail.com>
2020-08-14 15:19:43 -0400

    tuning pid for first bisque

---
+++ control/first-bisque.py
@@ -0,0 +1,48 @@
+import time
+import board
+import busio
+import digitalio
+import adafruit_max31855
+import RPi.GPIO as GPIO
+from pid import PID
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.D5)
+
+max31855 = adafruit_max31855.MAX31855(spi, cs)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)
+
+goal_temperature = 150
+period = 10
+
+pid = PID(1, 0, 0, 0, 1)
+last_good_temperature = 0
+
+while True:
+	try:
+		tempC = max31855.temperature
+
+		if tempC == 0:
+			raise RuntimeError('temperature is exactly zero, probably an issue')
+	except RuntimeError:
+		print('runtime error')
+	else:
+		tempF = tempC * 9 / 5 + 32
+		print(tempF)
+
+		last_good_temperature = tempF
+
+	pid_out = pid.update(last_good_temperature, goal_temperature, time.time())
+
+	time_on = period * pid_out
+	time_off = period - time_on
+
+	if time_off > 0:
+		GPIO.output(16, GPIO.LOW)
+		sleep(time_off)
+
+	if time_on > 0:
+		GPIO.output(16, GPIO.HIGH)
+		sleep(time_on)
