5b92ba3b705607f0852142bf47251258a44a476c
Sebastian Kimberk <skimberk1@gmail.com>
2020-08-24 17:43:58 -0400

    started working on io

--- control/io.py
+++ control/io.py
@@ -1,17 +1,33 @@
 import time
+import sched
+
 import PIL.ImageFont
 import luma.core.interface.serial
 import luma.oled.device
 import luma.core.render
+import RPi.GPIO
+
 import max31855
+import pid
 
 serial = luma.core.interface.serial.spi(port=0, device=0)
 device = luma.oled.device.ssd1351(serial)
 
+thermocouple = max31855.MAX31855(0, 1)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)
+
 fnt_53 = PIL.ImageFont.truetype("SourceCodeVariable-Roman.ttf", 53)
 fnt_24 = PIL.ImageFont.truetype("SourceCodeVariable-Roman.ttf", 20)
 
-thermocouple = max31855.MAX31855(0, 1)
+last_good_temp = 0
+
+def c_to_f(c):
+	return c * 9 / 5 + 32
+
+def update_loop():
+	
 
 while True:
 	temp_f = int(thermocouple.temperature * 9 / 5 + 32)
